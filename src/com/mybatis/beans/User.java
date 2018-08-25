810827272c418f1e429449f1eb9abadcc45ebd40
Ninewolf <x09wuhh@gmail.com>
2018-08-25 13:04:38 +0800

    mybatis学习，以及Idea整合GitHub测试

---
+++ src/com/mybatis/beans/User.java
@@ -0,0 +1,78 @@
+package com.mybatis.beans;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by lx on 2018/8/22.
+ */
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Integer uid;
+    private String username;
+    private String sex;
+    private Date birthday;
+    private String address;
+
+    public User() {
+    }
+
+    public User(String username, String sex, Date birthday, String address) {
+        this.username = username;
+        this.sex = sex;
+        this.birthday = birthday;
+        this.address = address;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uid=" + uid +
+                ", username='" + username + '\'' +
+                ", sex='" + sex + '\'' +
+                ", birthday=" + birthday +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
